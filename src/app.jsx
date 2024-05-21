043a0983e61ae2fce02da0a263d6ad647bccef1f
Bozhidar <bobi@microweber.com>
2024-05-21 20:03:52 +0300

    update

---
+++ src/app.jsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+import { createRoot } from 'react-dom/client';
+
+const domNode = document.getElementById('root');
+const root = createRoot(domNode);
+root.render(<h2>Hello from React!!!</h2>);
+
