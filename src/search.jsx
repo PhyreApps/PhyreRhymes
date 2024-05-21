1ba20b406e2b523db517eb3694b5a5e083812a27
Bozhidar <bobi@microweber.com>
2024-05-22 00:08:44 +0300

    update

--- src/search.jsx
+++ src/search.jsx
@@ -36,10 +36,10 @@ export default function Search() {
                             searchForRhymes();
                         }
                     }}
-                    className="w-full bg-white/10 focus:ring-indigo-500 focus:border-indigo-500 rounded-md shadow-sm sm:text-sm border-gray-300/10"
+                    className="w-full bg-white/10 placeholder-white/60 focus:ring-indigo-500 focus:border-indigo-500 rounded-md shadow-sm sm:text-sm border-gray-300/10"
                     placeholder="Напишете думата която искате да римувате..."
                 />
-                <button className="inline-flex justify-center rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white/90 text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500" onClick={function (e) {
+                <button className="inline-flex justify-center rounded-md border border-white/10 shadow-sm px-4 py-2 bg-white/10 text-sm font-medium text-white hover:bg-white/20 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500" onClick={function (e) {
                     searchForRhymes();
                 }}>
                     <MagnifyingGlassIcon className="h-5 w-5 text-gray-400" aria-hidden="true"/>
