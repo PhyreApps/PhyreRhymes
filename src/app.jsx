7ac65247d35d4a3eb9cbdd0582e04e8cfbd6263c
Bozhidar <bobi@microweber.com>
2024-05-21 22:20:51 +0300

    update

--- src/app.jsx
+++ src/app.jsx
@@ -7,10 +7,12 @@ const root = createRoot(domNode);
 
 root.render(
     <React.StrictMode>
-    <div className="w-[20rem] m-auto mt-12">
+    <div className="w-[24rem] m-auto mt-12">
         <div className="text-center mb-4">
             <h1 className="text-2xl">Phyre Rhymes v1.0</h1>
-            <p className="text-sm">A simple app to find rhyming words</p>
+            <p className="text-sm">
+                Речник за римуване на думи
+            </p>
         </div>
         <Search />
     </div>
--- src/search.jsx
+++ src/search.jsx
@@ -1,6 +1,14 @@
-import React from "react";
-import { MagnifyingGlassIcon } from '@heroicons/react/24/solid'
+import { MagnifyingGlassIcon } from '@heroicons/react/24/solid';
+import React from 'react';
+
 export default function Search() {
+
+    const [query, setQuery] = React.useState('');
+    const [loading, setLoading] = React.useState(false);
+    const [results, setResults] = React.useState([]);
+
+    
+
     return (
         <>
             <div className="mt-1 relative rounded-md shadow-sm">
@@ -8,12 +16,39 @@ export default function Search() {
                     type="text"
                     name="query"
                     id="query"
+                    onChange={function (e) {
+                        setQuery(e.target.value);
+                        setLoading(true);
+                    }}
                     className="focus:ring-indigo-500 focus:border-indigo-500 block w-full pr-10 sm:text-sm border-gray-300 rounded-md"
-                    placeholder="Search for a rhyme..."
+                    placeholder="Напишете думата която искате да римувате..."
                 />
                 <div className="absolute inset-y-0 right-0 pr-3 flex items-center pointer-events-none">
                     <MagnifyingGlassIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
                 </div>
+            </div>
+
+            <div>
+                <div>
+                    {loading ? <>
+
+                        <div>
+                            Търсене за: {query}
+                        </div>
+
+                        <div>
+                            {results.map((result) => (
+                                <div key={result.word}>
+                                    <p>{result.word}</p>
+                                </div>
+                            ))}
+                        </div>
+
+                    </> : <>
+
+                    </>}
+                </div>
+
             </div>
         </>
     )
