ef64bb94293fdb0cb2d221622887080b07f3de5b
Bozhidar <bobicloudvision@gmail.com>
2026-01-16 01:32:44 +0200

    Update syllables.go

--- rhyme-cli/stress/syllables.go
+++ rhyme-cli/stress/syllables.go
@@ -8,28 +8,87 @@ var vowels = map[rune]bool{
 	'ю': true, 'я': true,
 }
 
-// SplitSyllables разделя думата на срички
-func SplitSyllables(word string) []string {
-	var syllables []string
-	var current strings.Builder
-	runes := []rune(word)
+// isConsonant проверява дали руната е съгласна
+func isConsonant(r rune) bool {
+	return !vowels[r]
+}
 
-	for _, r := range runes {
-		current.WriteRune(r)
-		
-		// Ако е гласна, завършваме сричката
-		if vowels[r] {
-			syllables = append(syllables, current.String())
-			current.Reset()
-		}
+// SplitSyllables разделя думата на срички според българските правила за сричкопренасяне
+// Правила: всяка сричка трябва да има поне една гласна
+// Съгласните между гласните се разпределят: ако има една съгласна, тя отива към следващата сричка
+// Ако има две или повече съгласни, разделът обикновено е между тях
+// Неразделими групи съгласни (като "ст", "чк") остават заедно в една сричка
+func SplitSyllables(word string) []string {
+	if word == "" {
+		return []string{}
 	}
 
-	// Ако има останали букви, добавяме ги към последната сричка
-	if current.Len() > 0 && len(syllables) > 0 {
-		syllables[len(syllables)-1] += current.String()
-	} else if current.Len() > 0 {
-		// Ако няма срички, но има букви (рядко)
-		syllables = append(syllables, current.String())
+	runes := []rune(word)
+	if len(runes) == 0 {
+		return []string{}
+	}
+
+	var syllables []string
+	i := 0
+	
+	for i < len(runes) {
+		var current strings.Builder
+		
+		// Стъпка 1: Добавяме всички начални съгласни (ако има такива)
+		for i < len(runes) && isConsonant(runes[i]) {
+			current.WriteRune(runes[i])
+			i++
+		}
+		
+		// Стъпка 2: Добавяме гласната (задължителна за сричката)
+		if i < len(runes) && vowels[runes[i]] {
+			current.WriteRune(runes[i])
+			i++
+		}
+		
+		// Стъпка 3: Решаваме къде завършва сричката
+		// Гледаме напред: колко съгласни има до следващата гласна?
+		consonantCount := 0
+		j := i
+		for j < len(runes) && isConsonant(runes[j]) {
+			consonantCount++
+			j++
+		}
+		
+		// Ако няма повече гласни, всички останали съгласни отиват в тази сричка
+		if j >= len(runes) {
+			for i < len(runes) {
+				current.WriteRune(runes[i])
+				i++
+			}
+			if current.Len() > 0 {
+				syllables = append(syllables, current.String())
+			}
+			break
+		}
+		
+		// Правила за разпределение на съгласните:
+		if consonantCount == 0 {
+			// Няма съгласни между гласните - сричката завършва тук
+			if current.Len() > 0 {
+				syllables = append(syllables, current.String())
+			}
+		} else if consonantCount == 1 {
+			// Една съгласна - отива към следващата сричка (не я добавяме тук)
+			if current.Len() > 0 {
+				syllables = append(syllables, current.String())
+			}
+		} else {
+			// Две или повече съгласни
+			// Първата съгласна остава в тази сричка, останалите отиват в следващата
+			// Това е стандартното правило за българско сричкопренасяне
+			current.WriteRune(runes[i])
+			i++
+			
+			if current.Len() > 0 {
+				syllables = append(syllables, current.String())
+			}
+		}
 	}
 
 	return syllables
